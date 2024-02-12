0db1c20be9d72d4b97d9974f63e4e19fd5acd5bd
GiantProgrammer <bombaoyun111@gmail.com>
2024-02-12 18:03:45 +0300

    cors

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
+var cors = require("cors");
 
 
 
@@ -36,6 +37,8 @@ Object.keys(data).forEach(key => {
 
 const app = express();
 const router = require("./api/routes/product.js");
+
+app.use(cors());
 
 app.use('/addUser/:username',(req,res,next) => {
     const username = req.params.username;
